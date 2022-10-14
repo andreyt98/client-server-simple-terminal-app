368a4c99de9c61306efdb259848bec2b8fcfb4f5
andrey <andreytorrente@gmail.com>
2022-10-14 13:16:25 -0600

    initial commit

---
+++ cliente/Client.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("--------CLIENTE--------\n");
+Console.WriteLine("Envie mensajes o escriba 'exit' para finalizar...\n\n");
+
+IPAddress ipServerAddress = IPAddress.Parse("127.0.0.1");
+
+TcpClient tcpClient = new TcpClient();
+
+IPEndPoint ipEndPoint = new IPEndPoint(ipServerAddress, 3000);
+
+tcpClient.Connect(ipEndPoint);
+
+StreamWriter streamWriter = new StreamWriter(tcpClient.GetStream());
+
+String mensaje = "";
+
+while (mensaje != "exit") {
+    mensaje = Console.ReadLine();
+    streamWriter.WriteLine(mensaje);
+    streamWriter.Flush();
+}
---
+++ servidor/Server.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("--------SERVIDOR--------\n");
+Console.WriteLine("Esperando mensajes...\n\n");
+
+IPAddress hostEscuchando =  IPAddress.Parse("127.0.0.1");
+
+TcpListener canalDeEscucha = new TcpListener(hostEscuchando, 3000);
+
+Thread hiloDeEscucha = new Thread(new ThreadStart(accionHiloDeEscucha));
+
+hiloDeEscucha.Start();
+
+void accionHiloDeEscucha() {
+    canalDeEscucha.Start();
+    TcpClient tcpClient = canalDeEscucha.AcceptTcpClient();
+
+    StreamReader streamReader = new StreamReader(tcpClient.GetStream());
+
+    var datoRecibido = streamReader.ReadLine();
+
+    while (datoRecibido != "exit") {
+        Console.WriteLine("Nuevo mensaje: " + datoRecibido.ToUpper());
+        datoRecibido = streamReader.ReadLine();
+    }
+}
